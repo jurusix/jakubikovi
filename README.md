1a098eb4fcd4cee054f5635ba0b1ce36e97f08f4
Juraj Jakubik <juraj.jakubik@tieto.com>
2021-04-03 20:35:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# D &amp; J Wedding site
+
+[https://www.jakubikovi.cz](https://www.jakubikovi.cz)
---
+++ js/main.js
@@ -0,0 +1,64 @@
+;(function ($) {
+  'use strict'
+  // TOP Menu Sticky
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop()
+    if (scroll < 400) {
+      $('#sticky-header').removeClass('sticky')
+      $('#back-top').fadeIn(500)
+    } else {
+      $('#sticky-header').addClass('sticky')
+      $('#back-top').fadeIn(500)
+    }
+  })
+
+  $(document).ready(function () {
+    if ($(window).width() > 767) {
+      $('#map-iframe').attr(
+        'src',
+        'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ84aQ-dKQEkcR2HBv0OxCSGY&key=AIzaSyDqvcZbtfvXpL6eqUV95z1v3diMkSZD6dw'
+      )
+    }
+
+    $('#clock').countdown('2021/06/13 15:00:00', function (event) {
+      $(this).html(
+        event.strftime(
+          '<div class="countdown_wrap d-flex"><div  class="single_countdown"><h3>%D</h3><span>Dny</span></div><div class="single_countdown"><h3>%H</h3><span>Hodiny</span></div><div class="single_countdown"><h3>%M</h3><span>Minuty</span></div><div class="single_countdown"><h3>%S</h3><span>Sekundy</span></div></div>'
+        )
+      )
+    })
+
+    // mobile_menu
+    var menu = $('ul#navigation')
+    if (menu.length) {
+      menu.slicknav({
+        prependTo: '.mobile_menu',
+        closedSymbol: '+',
+        openedSymbol: '-',
+        closeOnClick: true,
+      })
+    }
+
+    /* magnificPopup img view */
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true,
+      },
+    })
+
+    /* magnificPopup video view */
+    $('.popup-video').magnificPopup({
+      type: 'iframe',
+    })
+
+    if (document.getElementById('default-select')) {
+      $('select').niceSelect()
+    }
+  })
+  //------- Mailchimp js --------//
+  function mailChimp() {
+    $('#mc_embed_signup').find('form').ajaxChimp()
+  }
+  mailChimp()
+})(jQuery)
